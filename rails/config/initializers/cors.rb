2a17a66b8e19a6a96803c3e36d982b99f34ab367
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-06-19 01:01:00 +0900

    rubocop修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -15,11 +15,11 @@
 #   end
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-        origins Settings.front_domain
-  
-        resource "*",
-               headers: :any,
-               methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins Settings.front_domain
+
+    resource "*",
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+end
